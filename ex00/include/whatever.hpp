ae7342969709072a7d593e166101088b2cf12262
Shetteemah <shettimaali58@yahoo.com>
2024-05-20 12:47:50 +0200

    headerr & functions created

---
+++ ex00/include/whatever.hpp
@@ -0,0 +1,34 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template<typename T>
+void swap(T &a, T &b)
+{
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+}
+
+template<typename T>
+const T &min(const T &a, const T &b)
+{
+    if (b >= a)
+        return (a);
+    return (b);
+}
+
+template<typename T>
+const T &max(const T &a, const T &b)
+{
+    if (b <= a)
+        return (a);
+    else
+        return (b);
+}
+
+#endif
